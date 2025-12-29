030197910f7607c6497842161b03b966db730858
manish <manishkumar20047877@gmail.com>
2025-12-29 19:42:35 +0530

    orders schema done

--- backend/schemas/OrdersSchema.jsx
+++ backend/schemas/OrdersSchema.jsx
@@ -1,1 +1,13 @@
 
+const { Schema } = require('mongoose');
+
+const OrdersSchema = new Schema({
+	name: "BHARTIARTL",
+	qty: 2,
+	avg: 538.05,
+	price: 541.15,
+	net: "+2.58%",
+	day: "+200.99%"
+});
+
+module.exports = OrdersSchema;
