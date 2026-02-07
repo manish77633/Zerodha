0e2b8777fc761cdbef64f8eb9b00a17f30e60a06
manish <manishkumar20047877@gmail.com>
2026-02-07 19:57:46 +0530

    handling 404

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    historyApiFallback: true, // ✅ This fixes 404 on dashboard reload
+  },
+  preview: {
+    port: 5173,
+    historyApiFallback: true, // ✅ This fixes 404 in production build
+  }
 })
